70cab9444c65833141b46404ad8d41814893f608
Jeff Hung <jeff_hung@trend.com.tw>
2012-06-05 22:20:51 +0800

    Executables don't have to put in $BUILD_PREFIX/usr/bin since homebrew will help create symlinks in #{HOMEBREW_PREFIX}/bin.

--- tme-mist-tools.rb
+++ tme-mist-tools.rb
@@ -12,6 +12,17 @@ class TmeMistTools < Formula
 
   def install
     system "make", "--directory=src/mist-tools", "BUILD_PREFIX=#{prefix}", "INSTALLPATH=", "CONFPATH=etc/tme", "LIBPATH=lib/tme-mist-tools", "install"
+
+    # Executables don't have to put in $BUILD_PREFIX/usr/bin since homebrew
+    # will help create symlinks in #{HOMEBREW_PREFIX}/bin.
+    mkdir "#{prefix}/bin"
+    mv "#{prefix}/usr/bin/mist-decode",  "#{prefix}/bin/"
+    mv "#{prefix}/usr/bin/mist-encode",  "#{prefix}/bin/"
+    mv "#{prefix}/usr/bin/mist-session", "#{prefix}/bin/"
+    mv "#{prefix}/usr/bin/mist-sink",    "#{prefix}/bin/"
+    mv "#{prefix}/usr/bin/mist-source",  "#{prefix}/bin/"
+    remove_dir "#{prefix}/usr/bin"
+    remove_dir "#{prefix}/usr"
   end
 end
 
--- tme-mist.rb
+++ tme-mist.rb
@@ -11,6 +11,13 @@ class TmeMist < Formula
 
   def install
     system "make", "--directory=src/mist", "BUILD_PREFIX=#{prefix}", "INSTALLPATH=", "CONFPATH=etc/tme", "LIBPATH=lib/tme-mist", "install"
+
+    # Executables don't have to put in $BUILD_PREFIX/usr/bin since homebrew
+    # will help create symlinks in #{HOMEBREW_PREFIX}/bin.
+    mv "#{prefix}/usr/bin/mist-line-gen",  "#{prefix}/bin/"
+    mv "#{prefix}/usr/bin/tme-console",    "#{prefix}/bin/"
+    remove_dir "#{prefix}/usr/bin"
+    remove_dir "#{prefix}/usr"
   end
 end
 
