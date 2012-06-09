753a4d09140b9e75ab62e15688d3123964530c96
Jeff Hung <jeff_hung@trend.com.tw>
2012-06-09 21:15:36 +0800

    Remove daemon.sh and add JAVA_HOME fix.

--- tme-common.rb
+++ tme-common.rb
@@ -9,10 +9,17 @@ class TmeCommon < Formula
 # depends_on 'rvm'
 
   def install
-    inreplace 'src/common/bin/create_zookeeper_nodes.sh', '/opt/trend/tme/lib', "#{lib}/tme"
-    inreplace 'src/common/bin/daemon.sh', '/var/run/tme', "#{var}/run/tme"
-    inreplace 'src/common/bin/daemon.sh', /^chown /, '#chown '
+    inreplace 'src/common/bin/create_zookeeper_nodes.sh', '/opt/trend/tme/lib', "#{lib}/tme-common"
+# Can't have these scripts since they depends on init.d infrastructure.
+#   inreplace 'src/common/bin/daemon.sh',                 '/var/run/tme',       "#{var}/run/tme"
+#   inreplace 'src/common/bin/daemon.sh',                 /^chown /,            '#chown '
+#   inreplace 'src/common/bin/daemon.sh',                 /^chown /,            '#chown '
+    inreplace 'src/common/conf/common/common-env.sh',     '/usr/java/latest',   '`/usr/libexec/java_home`'
+
     system "make", "--directory=src/common", "BUILD_PREFIX=#{prefix}", "INSTALLPATH=", "CONFPATH=etc/tme", "LIBPATH=lib/tme-common", "install"
+
+    # Can't have these scripts since they depends on init.d infrastructure.
+    remove     "#{bin}/daemon.sh"
   end
 end
 
