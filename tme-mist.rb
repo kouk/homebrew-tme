955e3e22831ebb2b5fbc4e733562c4f1ddbbb3b8
Jeff Hung <jeff.cc.hung@gmail.com>
2012-07-27 07:38:42 +0800

    Enhance previous fix since multiple JARs are duplicated.

--- tme-mist.rb
+++ tme-mist.rb
@@ -21,11 +21,15 @@ class TmeMist < Formula
     mv "#{prefix}/usr/bin/tme-console",    "#{prefix}/bin/"
     remove_dir "#{prefix}/usr/bin"
     remove_dir "#{prefix}/usr"
-    # Both common and mist modules of TME will install junit-3.8.1.jar.
-    # This is OK for TME since it use `cp -rf` when installing.
-    # But in Homebrew the final linking will fail due to existing link.
-    # Since they are same JAR anyway, we'll simply delete it here.
+
+    # Both common and mist modules depend on these JAR files that get installed
+    # to src/<module>/lib by Ivy. This is OK for TME since they are installed
+    # with `cp -rf`. But in Homebrew the final linking will fail due to
+    # existing symlinks. Since they are same JAR files anyway, we'll simply
+    # delete them here.
+    # See: https://github.com/trendmicro/tme/issues/11
     rm_f "#{prefix}/lib/tme/junit-3.8.1.jar"
+    rm_f "#{prefix}/lib/tme/jline-0.9.94.jar"
 
     # No need to have init.d scripts.
     remove_dir "#{prefix}/etc/init.d"
